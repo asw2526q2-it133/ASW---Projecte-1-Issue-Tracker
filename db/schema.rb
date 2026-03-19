de3da932dabf090f08e4d874f2d76705857cc48c
Roger Guinovart <roger.guinovart@estudiantat.upc.edu>
2026-03-19 18:55:35 +0100

    Scaffolds inicials user i issue

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2026_03_19_174513) do
+  create_table "issues", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "issue_type"
+    t.string "priority"
+    t.string "severity"
+    t.string "status"
+    t.string "subject"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_issues_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "issues", "users"
+end
