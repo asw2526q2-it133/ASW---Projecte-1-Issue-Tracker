ddd22ccc8d67b75e12e00b66800dacf6af3d9d0f
alex.michel.mediavilla <alex.michel.mediavilla@estudiantat.upc.edu>
2026-04-16 11:48:50 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
+# app/models/user.rb
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:github, :google_oauth2]
+         :recoverable, :validatable,
+         :omniauthable, omniauth_providers: [:github]
 
   has_many :issues, dependent: :destroy
 
   def self.from_omniauth(auth)
-    # Busca al usuario por provider y uid. Si no existe, lo crea.
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.full_name = auth.info.name
-    end
+      user.name = auth.info.name
+      user.full_name = auth.info.name 
+    end 
   end
 end
