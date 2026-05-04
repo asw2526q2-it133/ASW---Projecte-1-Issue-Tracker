a275e395f110b3f46c7b6bd9bb40a27fb4f5e732
alex.michel.mediavilla <alex.michel.mediavilla@estudiantat.upc.edu>
2026-05-04 16:17:16 +0200

    Afegir el controlador base de l'API amb el mètode d'autenticació per API Key

---
+++ app/controllers/api/application_controller.rb
@@ -0,0 +1,19 @@
+class Api::ApplicationController < ActionController::API
+  before_action :authenticate_api_key!
+
+  private
+
+  def authenticate_api_key!
+    api_key = request.headers['X-Api-Key'] # Extraiem la Api-Key del header
+    
+    @current_user = User.find_by(api_key: api_key) # Busquem l'usuari que tingui aquesta clau
+
+    unless @current_user
+      render json: { error: 'No autorizat. Api-Key invàlida o ausent' }, status: :unauthorized
+    end
+  end
+
+  def current_user
+    @current_user
+  end
+end
