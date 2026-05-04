f8feac6607c6874a619a6240f7f2e64b535bd62d
alex.michel.mediavilla <alex.michel.mediavilla@estudiantat.upc.edu>
2026-05-04 16:19:46 +0200

    Afegit el namespace d'API i les rutes pels endpoints d'issues

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
   end
   resources :users
   resources :profiles, only: [ :show, :edit, :update ]
+
+  namespace :api do
+    resources :issues
+  end 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
