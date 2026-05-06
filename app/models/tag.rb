4d88c2ca3eb738f950349389b96d096d1c1fab2a
alex.michel.mediavilla <alex.michel.mediavilla@estudiantat.upc.edu>
2026-05-06 02:15:29 +0200

    model modificat per al delete

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,25 @@
 class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  has_many :issues
+  
   has_many :issue_tags, dependent: :destroy
   has_many :issues, through: :issue_tags
+
+  before_destroy :prevent_deleting_last_one
+  before_destroy :check_for_associated_issues
+
+  private
+
+  def prevent_deleting_last_one
+    if Tag.count <= 1
+      errors.add(:base, "No es pot esborrar el darrer tag. Ha de quedar almenys un al sistema.")
+      throw :abort
+    end
+  end
+
+  def check_for_associated_issues
+    if issue_tags.any?
+      errors.add(:base, "El tag està en ús per una o més issues. Has de reassignar-ho a un altre tag abans d'esborrar-ho.")
+      throw :abort
+    end
+  end
 end
